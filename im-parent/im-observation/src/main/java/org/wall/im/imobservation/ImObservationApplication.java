37d8d912b3ef0e28026312ab0cfb0bde8e6b0009
Wall <785664704@qq.com>
2026-07-30 01:58:31 +0800

    提一版本整体的pom治理结果，尚未完成

---
+++ im-parent/im-observation/src/main/java/org/wall/im/imobservation/ImObservationApplication.java
@@ -0,0 +1,13 @@
+package org.wall.im.imobservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImObservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImObservationApplication.class, args);
+    }
+
+}
